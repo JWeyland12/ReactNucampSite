a256068acf41285b22df319a5223f8d6552d90a5
Jason Weyland <jason.weyland13@gmail.com>
2020-06-04 09:44:52 -0400

    React Router component

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -1,18 +1,17 @@
 import React from "react";
 import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from "reactstrap";
 
-    
-function RenderComments({comments}) {
+function RenderComments({ comments }) {
     if (comments) {
         return (
             <div className="col-md-5 m-1">
                 <h4>Comments</h4>
-                {comments.map((com) => {
+                {comments.map(comment => {
                     return (
-                        <div key={com.id}>
-                            <div>{com.text}</div>
+                        <div key={comment.id}>
+                            <div>{comment.text}</div>
                             <div>
-                                --{com.author}, {new Intl.DateTimeFormat("en-US", { year: "numeric", month: "short", day: "2-digit" }).format(new Date(Date.parse(com.date)))}
+                                --{comment.author}, {new Intl.DateTimeFormat("en-US", { year: "numeric", month: "short", day: "2-digit" }).format(new Date(Date.parse(comment.date)))}
                             </div>
                             <br />
                         </div>
@@ -24,34 +23,32 @@ function RenderComments({comments}) {
     return <div />;
 }
 
-function RenderCampsite({campsite}) {
-            return (
-                <div className="col-md-5 m-1">
-                    <Card>
-                        <CardImg top src={campsite.image} alt={campsite.name} />
-                        <CardBody>
-                            <CardTitle>{campsite.name}</CardTitle>
-                            <CardText>{campsite.description}</CardText>
-                        </CardBody>
-                    </Card>
-                </div>
-            );
-    }
+function RenderCampsite({ campsite }) {
+    return (
+        <div className="col-md-5 m-1">
+            <Card>
+                <CardImg top src={campsite.image} alt={campsite.name} />
+                <CardBody>
+                    <CardTitle>{campsite.name}</CardTitle>
+                    <CardText>{campsite.description}</CardText>
+                </CardBody>
+            </Card>
+        </div>
+    );
+}
 
-    function CampsiteInfo(props) {
-        const campsite = props.campsite;
-        if (campsite) {
-            return (
-                <div className="container">
-                    <div className="row">
-                        <RenderCampsite campsite={props.campsite}/>
-                        <RenderComments comments={props.campsite.comments}/>
-                    </div>
+function CampsiteInfo(props) {
+    if (props.campsite) {
+        return (
+            <div className="container">
+                <div className="row">
+                    <RenderCampsite campsite={props.campsite} />
+                    <RenderComments comments={props.comments} />
                 </div>
-            );
-        }
-        return <div />;
+            </div>
+        );
     }
-
+    return <div />;
+}
 
 export default CampsiteInfo;
