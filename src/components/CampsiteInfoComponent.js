ef49e1772e8809fba0898cd201d481f2c7361c03
Jason Weyland <jason.weyland13@gmail.com>
2020-06-18 09:07:33 -0400

    Fetch Post Comment

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -8,7 +8,7 @@ import { baseUrl } from "../shared/baseUrl";
 const maxLength = (len) => (val) => !val || val.length <= len;
 const minLength = (len) => (val) => val && val.length >= len;
 
-function RenderComments({ comments, addComment, campsiteId }) {
+function RenderComments({ comments, postComment, campsiteId }) {
     if (comments) {
         return (
             <div className="col-md-5 m-1">
@@ -24,7 +24,7 @@ function RenderComments({ comments, addComment, campsiteId }) {
                         </div>
                     );
                 })}
-                <CommentForm campsiteId={campsiteId} addComment={addComment} />
+                <CommentForm campsiteId={campsiteId} postComment={postComment} />
             </div>
         );
     }
@@ -82,7 +82,7 @@ function CampsiteInfo(props) {
                 </div>
                 <div className="row">
                     <RenderCampsite campsite={props.campsite} />
-                    <RenderComments comments={props.comments} addComment={props.addComment} campsiteId={props.campsite.id} />{" "}
+                    <RenderComments comments={props.comments} postComment={props.postComment} campsiteId={props.campsite.id} />{" "}
                 </div>
             </div>
         );
@@ -117,7 +117,7 @@ class CommentForm extends Component {
 
     handleSubmit(values) {
         this.toggleModal();
-        this.props.addComment(this.props.campsiteId, values.rating, values.author, values.text);
+        this.props.postComment(this.props.campsiteId, values.rating, values.author, values.text);
     }
 
     render() {
